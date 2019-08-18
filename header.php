d6b1043dad11da073675a8f87bfb05b01497975f
ryanchuah <ryan.chuah99@gmail.com>
2019-08-18 18:39:43 +0800

    dpisb final website

---
+++ header.php
@@ -0,0 +1,51 @@
+<header>
+                    <div id="logo">
+                        <a href="index.html"><img src="dpi logo.png" /></a>
+                    </div>
+
+                    <nav class="navbar">
+                        <ul>
+                            <a href="index.html"><li id="current-page">Home</li></a>
+                            <a href="about-us.html"><li>About Us</li></a>
+                            <div class="dropdown">
+                                <div class="dropbtn"><a href="Products/products.html" ><li>Products&ensp;<i class="fa fa-caret-down"></i></li></a>
+
+                                </div>
+                                <div class="dropdown-content">
+                                    <div class="grid-item bold"><a href="Products/products.html#BSAI">Busbar Support & Insulator</a></div>
+                                    <div class="grid-item bold"><a href="#">Current Transformers</a></div>
+                                    <div class="grid-item bold"><a href="Products/products.html#FS">Fuse Switch</a></div>
+                                    <div class="grid-item bold"><a href="Products/products.html#O">Others</a></div>
+                                    <ul class="categories">
+                                        <li><a href="Products/products.html#BS">Busbar Support</a></li>
+                                        <li><a href="Products/products.html#LS">L Shape</a></li>
+                                        <li><a href="Products/products.html#SI">Standoff Insulator</a></li>
+                                    </ul>
+
+                                    <ul class="categories">
+                                        <li><a href="#">EM Series</a></li>
+                                        <li><a href="#">CTME Series</a></li>
+                                        <li><a href="#">Resin Encapsulated</a></li>
+                                        <li><a href="#">Tape Wound</a></li>
+
+                                    </ul>
+
+                                    <ul class="categories">
+                                        <li><a href="Products/products.html#NFC">NFC</a></li>
+                                        <li><a href="Products/products.html#NFS">NFS</a></li>
+                                    </ul>
+
+                                    <ul class="categories">
+                                        <li><a href="Products/products.html#B">Bus Plug</a></li>
+                                         
+                                    </ul>
+                                </div>
+
+                            </div>
+
+
+                            <a href="#"><li>Downloads</li></a>
+                            <a href="#"><li>Contact</li></a>
+                        </ul>
+                    </nav>
+                </header>
---
+++ index2.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <head>
+        <title>DPI Industries Sdn Bhd</title>
+        <link href="stylesheetmain.css" type="text/css" rel="stylesheet"/>
+        <link href="stylesheetindex.css" type="text/css" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <link href="dpi logo - Copy.ico" rel="shortcut icon"/>
+        <link href="https://fonts.googleapis.com/css?family=Encode+Sans+Condensed|PT+Sans+Narrow" rel="stylesheet">
+         
+         
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+
+
+
+    <body>
+        <div id="container">
+            <div id="main">
+                <?php include 'header.php';?>
+                <div id="introduction">
+                    <div class="banner">
+                        <h1>DPI Industries<br>Sdn Bhd</h1>
+                        <p><br>Innovative Engineering for Value & Performance</p>
+                    </div> 
+                </div>
+
+                <div id="home-text">
+                    <div class="home-text-captions" id="why-choose-us">
+                        <h1>Why choose DPI Industries?</h1>
+                        <p>Here at DPI Industries, we prioritise customer satisfaction and guarantee world-class service. With over <span class="bold">20 years</span> of business experience and certifications from various bodies, we guarantee that our products are of the highest quality and precision.</p>  
+                    </div>
+
+                    <div class="home-text-captions" id="services">
+                        <h1>What are our services?</h1>
+                        <p>We manufacture low voltage switchgear and switchboard components.</p>
+                    </div>
+
+                    <div class="home-text-captions" id="product-quality">
+                        <h1>How are the quality of our products guaranteed?</h1>
+                        <p>Many of our products are tested and certified by independent type test authorities in order to meet international quality and safety standards. Our compliance to these standards is a key factor towards constant improvement of our business efficiency, product quality and customer service. The various bodies include the <span class="bold">National Association of Testing Authorities Australia</span>, <span class="bold">Majlis RekaBentuk Malaysia</span>, and the <span class="bold">New Zealand Industrial Design Council</span>. Furthermore, vigorous tests are carried out internally on our manufactured products to ensure safety and quality standards are upheld.</p>
+                    </div>
+                </div>  
+            </div>
+        </div>
+
+        <footer id="footer">
+            <div class="footer-captions" id="useful-links">
+                <h4>Terms of Use</h4>
+
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Downloads</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-captions" id="address">
+                <h4>Address</h4>
+                <p>DPI Industries Sdn. Bhd.,<br>34-5 Jalan Tasik Selatan 3,<br>57000 Kuala Lumpur</p>
+            </div>
+
+            <div class="footer-captions" id="working-hours">
+                <h4>Working Hours</h4>
+                <p>Weekdays: 8am to 6pm</p>
+            </div>
+
+            <div class="footer-captions" id="contact-us">
+                <h4>Contact Us</h4>
+                <ul>
+                    <li>Tel: +603-9058 3388</li>
+                    <li>Fax: +603-9057 2388</li>
+                    <li>Email: sales@dpisb.com</li>
+                </ul>
+            </div>
+        </footer>
+    </body>
+
+
+
+</html>
